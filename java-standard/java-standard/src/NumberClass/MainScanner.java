0a3968f4353bd27cd88b7dae5a56e21cad05feae
yogajulianp <yogajprasutiyo@gmail.com>
2022-12-15 11:54:07 +0700

    scanner hitung usia

--- java-standard/java-standard/src/NumberClass/MainScanner.java
+++ java-standard/java-standard/src/NumberClass/MainScanner.java
@@ -1,23 +1,73 @@
 package NumberClass;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
 public class MainScanner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yyyy");
+
         System.out.println("Masukkan Nama : ");
         String nama = scanner.nextLine();
+
         System.out.println("Masukkan tanggal Lahir");
         String tglLahir = scanner.nextLine();
+        Date tglLahirDate = formater.parse(tglLahir);
+        System.out.println("tgl Lahir : " + tglLahirDate);
 
         Date currentDate = new Date();
-        System.out.println(currentDate);
-
-        SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yyyy");
         String tglSekarang = formater.format(currentDate);
-        System.out.println("tgl Sekarang " + tglSekarang);
+        System.out.println("tgl Sekarang Date: " + currentDate);
+        System.out.println("tgl Sekarang : " + tglSekarang);
+
+
+        //hitung umur
+        Calendar calendar = Calendar.getInstance();
+        // tgl date menjadi calender
+        calendar.setTime(tglLahirDate);
+        System.out.println("calender tgl lahir" + calendar.getTime());
+        Calendar currentCalender = Calendar.getInstance();
+        currentCalender.setTime(new Date());
+        // cek calender sekarang
+        System.out.println("tgl sekarang :" + currentCalender.getTime());
+
+        // ambil selisih
+        //cari detik
+        Long time = (currentCalender.getTimeInMillis() - calendar.getTimeInMillis())/1000;
+        System.out.println("bentuk Second " + time + " detik");
+
+        //hour
+        Long hour = time/3600;
+        System.out.println("bentuk hour " + hour + " jam");
+
+        //day
+        Long day = hour/24;
+        System.out.println("bentuk day: " + day+ " hari");
+
+        //weeks
+        Long week = day/7;
+        System.out.println("bentuk weeks: " + week+ " pekan");
+
+        //Month
+        Long month = day/30;
+        System.out.println("format month: "+ month + " bulan");
+
+        //years
+        Long years = month/12;
+        System.out.println("bentuk years : "+ years + " tahun");
+
+        Long sisaBulan = month - years*12;
+
+        System.out.println("usia : " + years + " tahun, " + sisaBulan + " bulan");
+
+
+
+//        int tahunSekarang = calendar.get(Calendar.YEAR)
+//        int umur = Integer.parseInt(Waktusekarang) - WaktuLahir;
 
     }
 }
